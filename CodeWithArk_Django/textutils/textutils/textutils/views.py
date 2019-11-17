68314749e044137c3870aeff540d65ace18c5192
arkkhanu <arkkhanu@gmail.com>
2019-11-17 13:09:17 +0500

    Text Util has done

--- CodeWithArk_Django/textutils/textutils/textutils/views.py
+++ CodeWithArk_Django/textutils/textutils/textutils/views.py
@@ -8,11 +8,38 @@ def index(request):
 
 def analyze(request):
     djtext = request.GET.get('text', 'default')
-    removepunc = request.GET.get('removepunc', 'default')
-    analyzed = ""
-    punctuator = '''~_-!@#$%^&*()?><:;{}'''
-    for char in djtext:
-        if char not in punctuator:
-            analyzed += char
-    params = {'purpose': 'Remove Puncatuators', 'analyzed_text': analyzed}
-    return render(request, 'analyze.html', params)
+    removepunc = request.GET.get('removepunc', 'off')
+    fullcaps = request.GET.get('fullcaps', 'off')
+    newlineremover = request.GET.get('newlineremover', 'off')
+    extraspaceremover = request.GET.get('extraspaceremover', 'off')
+
+    if removepunc == 'on':
+        analyzed = ""
+        punctuator = '''~_-!@#$%^&*()?><:;{}'''
+        for char in djtext:
+            if char not in punctuator:
+                analyzed += char
+        params = {'purpose': 'Remove Puncatuators', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+    elif fullcaps == 'on':
+        analye = ""
+        for char in djtext:
+            analye += char.upper()
+        params = {'purpose': 'Upper Case', 'analyzed_text': analye}
+        return render(request, 'analyze.html', params)
+    elif newlineremover == 'on':
+        analyze = ""
+        for char in djtext:
+            if char != '\n':
+                analyze += char
+        params = {'purpose': 'New Line Remover', 'analyzed_text': analyze}
+        return render(request, 'analyze.html', params)
+    elif extraspaceremover == 'on':
+        analyze = ""
+        for index, char in enumerate(djtext):
+            if not (djtext[index] == ' ' and djtext[index + 1] == ' '):
+                analyze += char
+        params = {'purpose': 'New Line Remover', 'analyzed_text': analyze}
+        return render(request, 'analyze.html', params)
+    else:
+        return HttpResponse('Error')
